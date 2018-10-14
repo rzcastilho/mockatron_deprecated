58f3937e52f6fceaf724fc0ab3352ce59aec6f16
Rodrigo Zampieri Castilho <rodrigo.zampieri@gmail.com>
2018-10-13 22:11:51 -0300

    Env variable Heroku

--- mockatron/settings.py
+++ mockatron/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = 'staticfiles'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
